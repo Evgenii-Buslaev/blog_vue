fb5f8725d38378d7828c076fc397a9128ade28fd
Evgenii-Buslaev <eugenebus144@gmail.com>
2023-01-27 15:43:07 +0500

    Created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
-<template><h1>Блог</h1></template>
+<template>
+  <navigation-bar></navigation-bar>
+</template>
 
-<style lang="scss"></style>
+<script>
+import NavigationBar from "@/components/NavigationBar.vue";
+export default {
+  components: { NavigationBar },
+};
+</script>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@300;400;700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Source Code Pro", monospace;
+}
+.app {
+  padding: 20px;
+}
+</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar">
+    <a class="navbar__link">Главная</a>
+    <a class="navbar__link">Посты</a>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  width: 100%;
+  height: 60px;
+  background-color: rgba(0, 128, 128, 0.7);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.navbar__link {
+  font-size: 24px;
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
