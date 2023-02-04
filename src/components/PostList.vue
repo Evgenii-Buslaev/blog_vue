26eb73d5743fca5bc6416dc336dafae022f75882
EvgeniiBuslaev <eugenebus144@gmail.com>
2023-02-04 15:08:08 +0500

    added animation for creatind and deleting post

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="list">
-    <post-item
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @remove="$emit('remove', post)"
-    ></post-item>
+    <transition-group name="posts-list">
+      <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+      ></post-item>
+    </transition-group>
   </div>
 </template>
 
 <script>
+import { TransitionGroup } from "vue";
 import PostItem from "./PostItem.vue";
+
 export default {
-  components: { PostItem },
+  components: { PostItem, TransitionGroup },
   props: {
     posts: { type: Array, required: true },
   },
@@ -24,5 +28,19 @@ export default {
   width: 100%;
   height: auto;
   margin: 15px 0;
+}
+
+.posts-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.posts-list-enter-active,
+.posts-list-leave-active {
+  transition: all 0.3s ease;
+}
+.posts-list-enter-from,
+.posts-list-leave-to {
+  opacity: 0;
+  transform: translateX(100px);
 }
 </style>
